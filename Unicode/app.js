8748dbf1fc26200dfe3107cd844e5a0cee147577
EC Moises <222170@unamba.edu.pe>
2026-01-21 16:21:00 -0500

    Delete Unicode directory

--- Unicode/app.js
+++
@@ -1,7 +0,0 @@
-//[\$\€\£\¥]
-///[\s\p{Zs}]/gu
-///[\p{Script=Latin}\p{Script=Greek}]/gu
-///[\p{L}\p{N}]/gu
-let texto = "      abc12 3!@# $€$£¥ ΑλφαβητικόςabcΑΒΓαβγ";
-let expresion = /\p{Z}/gu;
-console.log(texto.match(expresion));
